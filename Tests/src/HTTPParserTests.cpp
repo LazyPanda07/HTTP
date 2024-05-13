e81442b348beca2a919ad4cb646d83c269194e38
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2024-05-13 20:24:55 +0500

    Refactor

--- Tests/src/HTTPParserTests.cpp
+++ Tests/src/HTTPParserTests.cpp
@@ -10,19 +10,19 @@ TEST(Parser, Request)
 	const json::JSONParser& jsonParser = parser.getJSON();
 	const web::HeadersMap& headers = parser.getHeaders();
 
-	ASSERT_EQ(parser.getMethod(), "POST");
-	ASSERT_EQ(parser.getParameters(), "post");
+	ASSERT_EQ(parser.getMethod(), "POST") << parser;
+	ASSERT_EQ(parser.getParameters(), "post") << parser;
 
-	ASSERT_EQ(headers.at("Host"), "httpbin.org");
-	ASSERT_EQ(headers.at("Connection"), "close");
-	ASSERT_EQ(headers.at("Accept"), "*/*");
-	ASSERT_EQ(headers.at("User-Agent"), "Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)");
-	ASSERT_EQ(headers.at("Content-Length"), "96");
+	ASSERT_EQ(headers.at("Host"), "httpbin.org") << parser;
+	ASSERT_EQ(headers.at("Connection"), "close") << parser;
+	ASSERT_EQ(headers.at("Accept"), "*/*") << parser;
+	ASSERT_EQ(headers.at("User-Agent"), "Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)") << parser;
+	ASSERT_EQ(headers.at("Content-Length"), "96") << parser;
 
-	ASSERT_EQ(jsonParser.getString("stringValue"), "qwe");
-	ASSERT_EQ(jsonParser.getInt("intValue"), 1500);
-	ASSERT_EQ(jsonParser.getDouble("doubleValue"), 228.322);
-	ASSERT_EQ(jsonParser.getNull("nullValue"), nullptr);
+	ASSERT_EQ(jsonParser.getString("stringValue"), "qwe") << parser;
+	ASSERT_EQ(jsonParser.getInt("intValue"), 1500) << parser;
+	ASSERT_EQ(jsonParser.getDouble("doubleValue"), 228.322) << parser;
+	ASSERT_EQ(jsonParser.getNull("nullValue"), nullptr) << parser;
 }
 
 TEST(Parser, Response)
@@ -30,17 +30,17 @@ TEST(Parser, Response)
 	web::HTTPParser parser(getHTTPResponse());
 	const web::HeadersMap& headers = parser.getHeaders();
 
-	ASSERT_EQ(parser.getHTTPVersion(), 1.1);
-	ASSERT_EQ(parser.getResponseCode(), web::responseCodes::ok);
-	ASSERT_EQ(parser.getResponseMessage(), "OK");
-	ASSERT_EQ(headers.at("Connection"), "Keep-Alive");
-	ASSERT_EQ(headers.at("Access-Control-Allow-Origin"), "*");
-	ASSERT_EQ(headers.at("Content-Encoding"), "gzip");
-	ASSERT_EQ(headers.at("Content-Type"), "text/html; charset=utf-8");
-	ASSERT_EQ(headers.at("Date"), "Wed, 10 Aug, 2016 13:17:18 GMT");
-	ASSERT_EQ(headers.at("Keep-Alive"), "timeout=5, max=999");
-	ASSERT_EQ(headers.at("Server"), "Apache");
-	ASSERT_EQ(headers.at("X-Frame-Options"), "DENY");
+	ASSERT_EQ(parser.getHTTPVersion(), 1.1) << parser;
+	ASSERT_EQ(parser.getResponseCode(), web::responseCodes::ok) << parser;
+	ASSERT_EQ(parser.getResponseMessage(), "OK") << parser;
+	ASSERT_EQ(headers.at("Connection"), "Keep-Alive") << parser;
+	ASSERT_EQ(headers.at("Access-Control-Allow-Origin"), "*") << parser;
+	ASSERT_EQ(headers.at("Content-Encoding"), "gzip") << parser;
+	ASSERT_EQ(headers.at("Content-Type"), "text/html; charset=utf-8") << parser;
+	ASSERT_EQ(headers.at("Date"), "Wed, 10 Aug, 2016 13:17:18 GMT") << parser;
+	ASSERT_EQ(headers.at("Keep-Alive"), "timeout=5, max=999") << parser;
+	ASSERT_EQ(headers.at("Server"), "Apache") << parser;
+	ASSERT_EQ(headers.at("X-Frame-Options"), "DENY") << parser;
 }
 
 TEST(Parser, CONNECT)
@@ -48,10 +48,10 @@ TEST(Parser, CONNECT)
 	web::HTTPParser parser(getCONNECTRequest());
 	const web::HeadersMap& headers = parser.getHeaders();
 
-	ASSERT_EQ(parser.getParameters(), "server.example.com:80");
+	ASSERT_EQ(parser.getParameters(), "server.example.com:80") << parser;
 
-	ASSERT_EQ(headers.at("Host"), "server.example.com:80");
-	ASSERT_EQ(headers.at("Proxy-Authorization"), "basic aGVsbG86d29ybGQ=");
+	ASSERT_EQ(headers.at("Host"), "server.example.com:80") << parser;
+	ASSERT_EQ(headers.at("Proxy-Authorization"), "basic aGVsbG86d29ybGQ=") << parser;
 }
 
 TEST(Parser, Streams)
@@ -70,9 +70,9 @@ TEST(Parser, Streams)
 
 		data = os.str();
 
-		ASSERT_NE(data.find("CONNECT server.example.com:80 HTTP/1.1\r\n"), npos);
-		ASSERT_NE(data.find("Host: server.example.com:80\r\n"), npos);
-		ASSERT_NE(data.find("Proxy-Authorization: basic aGVsbG86d29ybGQ=\r\n"), npos);
+		ASSERT_NE(data.find("CONNECT server.example.com:80 HTTP/1.1\r\n"), npos) << parser;
+		ASSERT_NE(data.find("Host: server.example.com:80\r\n"), npos) << parser;
+		ASSERT_NE(data.find("Proxy-Authorization: basic aGVsbG86d29ybGQ=\r\n"), npos) << parser;
 	}
 
 	{
@@ -87,10 +87,10 @@ TEST(Parser, Streams)
 
 		data = os.str();
 
-		ASSERT_NE(data.find("GET /search?q=test HTTP/2\r\n"), npos);
-		ASSERT_NE(data.find("Host: www.bing.com\r\n"), npos);
-		ASSERT_NE(data.find("User-Agent: curl/7.54.0\r\n"), npos);
-		ASSERT_NE(data.find("Accept: */*\r\n"), npos);
+		ASSERT_NE(data.find("GET /search?q=test HTTP/2\r\n"), npos) << parser;
+		ASSERT_NE(data.find("Host: www.bing.com\r\n"), npos) << parser;
+		ASSERT_NE(data.find("User-Agent: curl/7.54.0\r\n"), npos) << parser;
+		ASSERT_NE(data.find("Accept: */*\r\n"), npos) << parser;
 	}
 }
 
@@ -98,5 +98,5 @@ TEST(Parser, Parameters)
 {
 	web::HTTPParser parser(getGetRequest());
 
-	ASSERT_EQ(parser.getKeyValueParameters().at("q"), "test");
+	ASSERT_EQ(parser.getKeyValueParameters().at("q"), "test") << parser;
 }
